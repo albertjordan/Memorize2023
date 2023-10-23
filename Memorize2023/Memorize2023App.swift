49e92eb677a908677efa39ca993961906fe8031f
ALBERT JORDAN <albertjordan@mac.com>
2023-10-22 17:15:29 -0700

    Initial Commit

---
+++ Memorize2023/Memorize2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2023App.swift
+//  Memorize2023
+//
+//  Created by Albert Jordan on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
